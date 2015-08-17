60f8e254e6e69c7b60a8b2f92cef6cb241bf38e4
Simon <simonthechain@gmail.com>
2015-08-17 03:23:31 -0400

    Update_Lorentz_Calculation_12
    Added some loops, will add more

--- Lorentz_Calculation.py
+++ Lorentz_Calculation.py
@@ -1,16 +1,18 @@
+# These modules are needed to do the square root operation and to display the results with precision
+from math import sqrt
+from decimal import Decimal
+
 # This sets the loop for the whole script
 program = True
 while program == True:
+
 
 	print """
 This will calculate some of the effects of Einstein's Theory of Special Relativity.
 """
 
-# These modules are needed to do the square root operation and to display the results with precision
-	from math import sqrt
-	from decimal import Decimal
 
-# This is to make sure a number was entered
+# This function verifies that a number was entered
 	def number_check(x):
 		try:
 			int(x)
@@ -18,52 +20,70 @@ This will calculate some of the effects of Einstein's Theory of Special Relativi
 			try:
 				float(x)
 			except ValueError:
-				exit = raw_input("This is not a number.  Press Enter.")
-			exit == quit()
-	
-		
+				print "This is not a number.  Try again."
+				global num_check
+				num_check = False
+
+				
 # This is where the user selects the units to be used
-	unit_velocity = raw_input("""Select the unit to be used for speed:
+	question1 = True
+	while question1 == True:
+		print """Select the unit to be used for speed:
 (1) Kilometers per hour (km/h)
 (2) Miles per hour (mph)
-(3) Fraction of the speed of light (ex.: 0.85c)
-> """)
+(3) Fraction of the speed of light (ex.: 0.85c)"""
+		unit_velocity = raw_input("> ")
 
 # This checks which unit was selected and sets c accordingly
-	if str(unit_velocity) == "1":
-		unit_velocity = "km/h"
-		c = Decimal(1079252848800)
-	elif str(unit_velocity) == "2":
-		unit_velocity = "mph"
-		c = Decimal(670616629)
-	elif str(unit_velocity) == "3":
-		unit_velocity = "c"
-		c = Decimal(1)
-	else:
-		exit = raw_input("Expected 1, 2 or 3.  Press Enter.")
-		exit == quit()
-
+		if str(unit_velocity) == "1":
+			unit_velocity = "km/h"
+			c = Decimal(1079252848800)
+			question1 = False
+		elif str(unit_velocity) == "2":
+			unit_velocity = "mph"
+			c = Decimal(670616629)
+			question1 = False
+		elif str(unit_velocity) == "3":
+			unit_velocity = "c"
+			c = Decimal(1)
+			question1 = False
+		else:
+			subquestion1 = True
+			while subquestion1 == True:
+				print "Expected 1, 2 or 3.  Try again? (y/n)"
+				question1_answer = raw_input("> ").lower()
+				if question1_answer[:1] == "y":
+					print "Ok"
+					subquestion1 = False
+				elif question1_answer[:1] == "n":
+					quit()
+				else:
+					print "Input unrecognized, try again."
 	
-# This is where the user inputs the value for the velocity
-	print "Enter your speed in %s (the speed of light in a vacuum is %s %s): " % (unit_velocity, c, unit_velocity)
-	speed = raw_input("> ")
-
-	number_check(speed)
-
-	velocity = Decimal(speed)
+	
+# This is where the user inputs the value for the velocity	
+	question2 = True
+	while question2 == True:
+		num_check_loop = True
+		while num_check_loop == True:
+			print "Enter your speed in %s (the speed of light in a vacuum is %s %s): " % (unit_velocity, c, unit_velocity)
+			speed = raw_input("> ")
+			num_check = True
+			number_check(speed)
+			if num_check == True:
+				num_check_loop = False
 
 # This verifies that the number entered is valid
-	if velocity > c:
-		exit = raw_input("You are moving faster than light! This is impossible.  Press Enter.")
-		exit == quit()
-	elif velocity == c:
-		exit = raw_input("You are travelling at exactly the speed of light, this is not allowed.  Press Enter.")
-		exit == quit()
-	elif velocity <= 0:
-		exit = raw_input("You need to be in foward motion. Press Enter.")
-		exit == quit()
-	else:
-		print "Ok"
+		velocity = Decimal(speed)
+		if velocity > c:
+			print "You are moving faster than light! This is impossible.  Try again."
+		elif velocity == c:
+			print "You are travelling at exactly the speed of light, this is not allowed."
+		elif velocity <= 0:
+			print "You need to be in foward motion."
+		else:
+			question2 = False
+			print "Ok"
 
 
 # This is where the user inputs the value for the mass	
@@ -134,11 +154,11 @@ If you travel lying down in the direction of movement, to someone not moving you
 """ % ((mass * gamma), (length / gamma), (1 * gamma))
 
 # This asks to run the script again
-	print "Do you want to run the program again? (Yes/No)"
+	print "Do you want to enter new values? (Yes/No)"
 	rerun = raw_input("> ").lower()
 	if rerun == "yes" or rerun == "y":
 		print "Ok"
 	else:
 		program = False
-		exit = raw_input("Thank you for using this program.  Press Enter.")
+		exit = raw_input("Thank you for using this program by Simon Lachaine.")
 		exit == quit()
